d591f39c71cf1349776cd4b0f8e9a3fc81d32bff
olgapf02 <olgapf02@gmail.com>
2023-03-01 14:04:36 +0100

    slugify

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const slugify = require('slugify')
 const products_routes = require('./routes/products.js')
 
 //Server instantiation
 const app = express()
+const PORT = process.env.PORT || 3030;   
 
 //Server configuration: template engine
 app.set('views', './views');
@@ -14,8 +16,14 @@ app.use(express.json())
 app.use('/', products_routes)
 
 //Server startup
-app.listen(5000, () => {
-    console.log('server is listening on port 5000')
-})
+// app.listen(5000, () => {
+//     console.log('server is listening on port 5000')
+// })
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+  });
 
+const mensajeBienvenida = '¡Bienvenido al servidor Express!';
+const mensajeSlug = slugify(mensajeBienvenida, { replacement: '*', lower: true });
 
+console.log(mensajeSlug); // salida: bienvenido-al-servidor-express! 
